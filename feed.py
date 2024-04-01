ea65836cbfb1ab457a8068d8aed5a6d2597d2322
Datocrat One <12563261+datocrats-org@users.noreply.github.com>
2024-04-01 20:38:46 +0000

    fix typo, regenerate podcast.xml

--- feed.py
+++ feed.py
@@ -7,7 +7,7 @@ with open('feed.yaml') as file:
     rss_element = xml_tree.Element('rss',{
         'version':'2.0',
         'xmlns:itunes':'http://www.itunes.com/dtds/podcast-1.0.dtd',
-        'xlmns:content':'http://purl.org/rss/1.0/modules/content'
+        'xmlns:content':'http://purl.org/rss/1.0/modules/content'
     })
 
 channel_element = xml_tree.SubElement(rss_element,'channel')
